f3bc9af74b7b0c4c4ba868ee87390f391b522480
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-12-14 01:09:17 +0100

    re-layouting the transaction card

--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -13,86 +13,92 @@ class TransactionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.white,
+      color: Theme.of(context).colorScheme.primary,
       margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
       elevation: 10,
       child: Padding(
-        padding: const EdgeInsets.only(top: 12.0, left: 12.0, right: 12.0),
+        padding: const EdgeInsets.all(8.0),
         child: SizedBox(
           width: 200,
-          child: Column(
+          child: Row(
             mainAxisSize: MainAxisSize.max,
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  transaction.title,
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  DateFormat("dd.MM.yyyy EEEE", "de")
-                      .format(transaction.dateOfTransaction)
-                      .toString(),
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        color: Colors.grey,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      transaction.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      DateFormat("dd.MM.yyyy EEEE", "de")
+                          .format(transaction.dateOfTransaction)
+                          .toString(),
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Colors.grey,
+                          ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Provider.of<TransactionProvider>(context,
+                                  listen: false)
+                              .removeTransaction(transaction);
+                        },
+                        icon: const Icon(Icons.delete_forever),
+                        splashRadius: 20,
                       ),
-                ),
+                      ...transaction.tags.map((element) {
+                        return SizedBox(
+                          child: Card(
+                            color: Theme.of(context).colorScheme.primary,
+                            elevation: 8,
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 3.0,
+                                horizontal: 10,
+                              ),
+                              child: Text(
+                                element,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                    ],
+                  ),
+                ],
               ),
               Align(
                 alignment: Alignment.centerRight,
                 child: Text(
                   transaction.amount > 0
-                      ? "+${transaction.amount.toString()} €"
-                      : "${transaction.amount.toString()} €",
-                  style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                      ? "+${((transaction.amount * 100).round() / 100).toString()} €"
+                      : "${((transaction.amount * 100).round() / 100).toString()} €",
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
                         color: transaction.amount > 0
                             ? Colors.lightGreenAccent.shade400
                             : Colors.red.shade700,
                         fontWeight: FontWeight.w400,
                       ),
                 ),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Provider.of<TransactionProvider>(context, listen: false)
-                          .removeTransaction(transaction);
-                    },
-                    icon: const Icon(Icons.delete_forever),
-                    splashRadius: 20,
-                  ),
-                  ...transaction.tags.map((element) {
-                    return SizedBox(
-                      child: Card(
-                        color: Theme.of(context).colorScheme.primary,
-                        elevation: 8,
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 3.0,
-                            horizontal: 10,
-                          ),
-                          child: Text(
-                            element,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }),
-                ],
               ),
             ],
           ),
